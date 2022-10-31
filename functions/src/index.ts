2c6e42144ab47324cbaad1b41ebce3ff3c3a281e
unknown <gustavo.silva@ear.com.br>
2022-10-31 02:16:45 -0300

    :data: data: writting matches

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
